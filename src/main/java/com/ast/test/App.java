6ca6483c39c87fb44b6ed63a87bc4470d308d35d
Arjun <sta@myself.com>
2017-12-09 22:17:22 -0500

    Initial commit for project

---
+++ src/main/java/com/ast/test/App.java
@@ -0,0 +1,13 @@
+package com.ast.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
